42ca3147e6cafae127d7abdf4c3c3c303dfe5de3
Lila Ramani <LRamani12@gmail.com>
2018-05-21 13:52:52 -0400

    added experimental nav

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+	console.log('here');
+	$("#merchButton").click(function (){
+	    $('html, body').animate({
+	        scrollTop: $("#merch").offset().top
+	    }, 2000);
+	});
+
+	$("#showButton").click(function (){
+	    $('html, body').animate({
+	        scrollTop: $("#tour").offset().top
+	    }, 2000);
+	});
+
+	$("#contactButton").click(function (){
+	    $('html, body').animate({
+	        scrollTop: $("#contact").offset().top
+	    }, 2000);
+	});
+});
